33cb45f12fabb9080d2f01c0e005d3291a44db6a
Jbierenbroodspot <jim12.brood@gmail.com>
2023-02-15 15:45:41 +0100

    Implement PerceptronNetwork.predict()

--- p1_perceptron/main.py
+++ p1_perceptron/main.py
@@ -41,6 +41,14 @@ class PerceptronLayer:
         return f"PerceptronLayer with {len(self.perceptrons)} perceptrons"
 
     def activate(self, input_values: list[float]) -> list[float]:
+        """Activate every perceptron within the layer using the input_values.
+
+        Args:
+            input_values: Float values used to calculate activation of the perceptrons.
+
+        Returns:
+            The output for every perceptron within the layer.
+        """
         return [perceptron.activate(input_values) for perceptron in self.perceptrons]
 
 
@@ -53,8 +61,27 @@ class PerceptronNetwork:
     def __str__(self) -> str:
         return f"PerceptronNetwork with {len(self.layers)} layers"
 
-    def predict(input_values: list[float]) -> list[float]:
-        return []
+    def predict(self, input_values: list[float]) -> list[float]:
+        """Calculates the output of of multiple layers of Perceptrons.
+
+        The input_values are fed into the first layer and the result is then fed into the next layer until the last
+        layer, of which the output values get returned as the prediction.
+
+        The name of the method can be a bit misleading but it is called this way to mimic the `predict()` methods for
+        models within the sk_learn library.
+
+        Args:
+            input_values: The initial values of which to predict the output of.
+
+        Returns:
+            The output of the last layer after it has been fed by the layer before that.
+        """
+        output_values: list[float] = input_values
+
+        for layer in self.layers:
+            output_values = layer.activate(output_values)
+
+        return output_values
 
 
 def main() -> None:
