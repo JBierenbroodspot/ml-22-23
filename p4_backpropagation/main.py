c884ee31558f7778c2a2b903aacff371507d9ccb
Jbierenbroodspot <jim12.brood@gmail.com>
2023-03-16 22:54:49 +0100

    Setup empty classes for p4

--- p4_backpropagation/main.py
+++ p4_backpropagation/main.py
@@ -1,12 +1,12 @@
-from p3_sigmoid_neuron import Neuron, NeuronNetwork, sigmoid_activation
+from ml.neurons import Neuron, NeuronNetwork
 
 
 class OutputNeuron(Neuron):
-    activation_function = sigmoid_activation
+    ...
 
 
 class HiddenNeuron(Neuron):
-    activation_function = sigmoid_activation
+    ...
 
 
 class NeuronNetwork(NeuronNetwork):
