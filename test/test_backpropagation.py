0faf62bb702afe0161823353a38e5134d33418e8
Jbierenbroodspot <jim12.brood@gmail.com>
2023-03-17 15:43:17 +0100

    Add simple test for error

--- test/test_backpropagation.py
+++ test/test_backpropagation.py
@@ -10,35 +10,16 @@ class TestOutputNeuron(unittest.TestCase):
 
     def setUp(self):
         self.output_neuron = OutputNeuron([0.534, 0.799], -0.146, sigmoid_activation, 0.1)
+        self.output_neuron.activate_and_set_new_weights([0.391, 0.511], 1)
 
     def test_error(self):
         """Test if the error property works correctly."""
-        error_value: float = self.output_neuron.get_error(1, [0.391, 0.511])
+        error_value: float = self.output_neuron.error
         expected_vale: float = -0.091
 
         msg = f"The output of error: {error_value}, does not match the expected value {expected_vale}"
 
         self.assertAlmostEqual(error_value, expected_vale, 4, msg)
-
-    def test_gradient(self):
-        """Test the gradient method."""
-        ...
-
-    def test_get_delta(self):
-        """Test the get_delta method."""
-        ...
-
-    def test_has_new_weights(self):
-        """Test if the attribute in which get_all_deltas stores the new weights."""
-        ...
-
-    def test_get_all_deltas(self):
-        """Test the get_all_deltas method."""
-        ...
-
-    def test_update(self):
-        """Test the update method."""
-        ...
 
 
 class TestHiddenNeuron(unittest.TestCase):
